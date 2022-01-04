7b266195dcfcd5cfe150ba808570458f50234cfe
GCPhillips <grant.christopher.phillips@gmail.com>
2022-01-03 23:09:59 -0600

    feat: added example for default profiles

--- src/main/java/com/grantphillips/sfgdi/SfgDiApplication.java
+++ src/main/java/com/grantphillips/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package com.grantphillips.sfgdi;
 
-import com.grantphillips.sfgdi.controllers.ConstructorInjectedController;
-import com.grantphillips.sfgdi.controllers.MyController;
-import com.grantphillips.sfgdi.controllers.PropertyInjectedController;
-import com.grantphillips.sfgdi.controllers.SetterInjectedController;
+import com.grantphillips.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
