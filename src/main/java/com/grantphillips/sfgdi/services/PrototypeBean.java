5928a4ee9e52ca6066a9f4526bf00d3e2141d6e5
GCPhillips <grant.christopher.phillips@gmail.com>
2022-01-16 15:07:35 -0600

    feat: adding examples for Prototype and Singleton bean scopes

--- src/main/java/com/grantphillips/sfgdi/services/PrototypeBean.java
+++ src/main/java/com/grantphillips/sfgdi/services/PrototypeBean.java
@@ -1,7 +1,10 @@
 package com.grantphillips.sfgdi.services;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Component
 public class PrototypeBean {
 
